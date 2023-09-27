f8079f8ab182e1c30da55410446b23f16f98df3a
Alexander Apaza <alrus2797@gmail.com>
2023-09-27 02:15:48 -0500

    feat: add endpoints, command & queries to create votes

---
+++ DC.api/src/Application/Votes/Commands/CreateVote/CreateVote.cs
@@ -0,0 +1,104 @@
+﻿using System.Net;
+using DC.api.Application.Common.Interfaces;
+using DC.api.Domain.Entities;
+
+namespace DC.api.Application.Votes.Commands.CreateVote;
+
+public record CreateVoteCommand : IRequest<VoteDto>
+{
+    public int PollId { get; init; }
+    public int ChoiceId { get; init; }
+    public string? Author { get; init; }
+}
+
+public class CreateVoteCommandValidator : AbstractValidator<CreateVoteCommand>
+{
+    public CreateVoteCommandValidator()
+    {
+        RuleFor(v => v.PollId)
+            .NotEmpty();
+        RuleFor(v => v.ChoiceId)
+            .NotEmpty();
+        RuleFor(v => v.Author)
+            .MaximumLength(200)
+            .NotEmpty();
+    }
+}
+
+public class CreateVoteCommandHandler : IRequestHandler<CreateVoteCommand, VoteDto>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateVoteCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<VoteDto> Handle(CreateVoteCommand request, CancellationToken cancellationToken)
+    {
+        var _poll = await _context.Polls
+            .Include(p => p.PollChoices)
+            .FirstOrDefaultAsync(p => p.Id == request.PollId, cancellationToken);
+
+        if (_poll == null)
+        {
+            throw new NotFoundException(nameof(Domain.Entities.Poll), request.PollId.ToString());
+        }
+
+        Console.WriteLine("poll fetched");
+
+        var _pollChoice = _poll.PollChoices.FirstOrDefault(p => p.Id == request.ChoiceId);
+
+
+        if (_pollChoice == null)
+        {
+            throw new NotFoundException(nameof(Domain.Entities.PollChoice), request.ChoiceId.ToString());
+        }
+
+        Console.WriteLine("Poll choice fetched");
+
+
+        var _existingVote = await _context.Votes
+            .FirstOrDefaultAsync(v => v.PollId == request.PollId && v.Author == request.Author, cancellationToken);
+
+
+
+        if (_existingVote != null)
+        {
+            throw new HttpRequestException(
+                "You have already voted for this poll.",
+                null,
+                HttpStatusCode.BadRequest
+            );
+        }
+        Console.WriteLine("Not vote exists");
+
+
+        _pollChoice.NumberOfVotes++;
+
+        Console.WriteLine("Increased number of votes");
+
+        var _vote = new Vote
+        {
+            PollId = request.PollId,
+            ChoiceId = request.ChoiceId,
+            Author = request.Author
+        };
+
+        _context.Votes.Add(_vote);
+
+        Console.WriteLine("Added vote");
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        Console.WriteLine("Saved changes");
+
+        return new VoteDto
+        {
+            Id = _vote.Id,
+            PollId = _vote.PollId,
+            ChoiceId = _vote.ChoiceId,
+            Author = _vote.Author
+        };
+    }
+}
---
+++ DC.api/src/Application/Votes/Queries/GetVotedChoice/GetVotedChoice.cs
@@ -0,0 +1,58 @@
+﻿using DC.api.Application.Common.Interfaces;
+using DC.api.Application.Polls.Queries.GetPolls;
+
+namespace DC.api.Application.Votes.Queries.GetVotedChoice;
+
+public record GetVotedChoiceQuery : IRequest<List<PollChoiceDisplayDto>>
+{
+    public int PollId { get; init; }
+    public string? Author { get; init; }
+}
+
+public class GetVotedChoiceQueryValidator : AbstractValidator<GetVotedChoiceQuery>
+{
+    public GetVotedChoiceQueryValidator()
+    {
+    }
+}
+
+public class GetVotedChoiceQueryHandler : IRequestHandler<GetVotedChoiceQuery, List<PollChoiceDisplayDto>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetVotedChoiceQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<PollChoiceDisplayDto>> Handle(GetVotedChoiceQuery request, CancellationToken cancellationToken)
+    {
+
+        var _vote = await _context.Votes
+            .FirstOrDefaultAsync(v => v.PollId == request.PollId && v.Author == request.Author, cancellationToken);
+
+        if (_vote == null)
+        {
+            return new List<PollChoiceDisplayDto>();
+        }
+
+        var _pollChoice = await _context.PollChoices
+            .FirstOrDefaultAsync(p => p.Id == _vote.ChoiceId, cancellationToken);
+
+        if (_pollChoice == null)
+        {
+            return new List<PollChoiceDisplayDto>();
+        }
+
+        return new List<PollChoiceDisplayDto>{
+            new PollChoiceDisplayDto{
+                Id = _pollChoice.Id,
+                Title = _pollChoice.Title,
+                Description = _pollChoice.Description,
+                NumberOfVotes = _pollChoice.NumberOfVotes
+            }
+        };
+
+
+    }
+}
